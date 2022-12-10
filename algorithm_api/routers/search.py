80c5c7b7de4f75f199cdb2836accf48fe80416d0
Stephen Katz <stephenjk41@gmail.com>
2022-12-09 21:11:13 -0500

    add steps to bin search

--- algorithm_api/routers/search.py
+++ algorithm_api/routers/search.py
@@ -8,6 +8,7 @@ router = APIRouter()
 async def run_binary_search_sorted(arr: list[int], target: int):
     # Python3 Program for recursive binary search.
     # Returns index of x in arr if present, else -1
+    step = []
     def binary_search(arr, left, right, x, count):
         # Check base case
         count += 1
@@ -15,14 +16,17 @@ async def run_binary_search_sorted(arr: list[int], target: int):
             mid = left + (right - left) // 2
             # If element is present at the middle itself
             if arr[mid] == x:
+                step.append({arr[mid]})
                 return mid, count
             # If element is smaller than mid, then it
             # can only be present in left subarray
             elif arr[mid] > x:
+                step.append({arr[left], (arr[mid-1])})
                 return binary_search(arr, left, mid - 1, x, count)
             # Else the element can only be present
             # in right subarray
             else:
+                step.append({arr[left], (arr[mid+1])})
                 return binary_search(arr, mid + 1, right, x, count)
         else:
             # Element is not present in the array
@@ -31,6 +35,6 @@ async def run_binary_search_sorted(arr: list[int], target: int):
     result, count = binary_search(arr, 0, len(arr) - 1, target, 0)
 
     if result != -1:
-        return f"Element is present at index {result} in {count} steps"
+        return {"text": f"Element is present at index {result} in {count} steps", "steps": step}
     else:
         return "Element is not present in array"
